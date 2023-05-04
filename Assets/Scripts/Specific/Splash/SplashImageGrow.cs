b7bcf5db4209507f81fa437714ef8677d95928c0
Marcelo Petrucelli <m2petrucelli@gmail.com>
2023-05-04 00:54:31 -0300

    Quickfix de acesso indevido à animações pós mudança de cena.

--- Assets/Scripts/Specific/Splash/SplashImageGrow.cs
+++ Assets/Scripts/Specific/Splash/SplashImageGrow.cs
@@ -9,7 +9,9 @@ public class SplashImageGrow : MonoBehaviour
     [SerializeField, MinMaxSlider(0f, 10f)] Vector2 xVariation = new(0.8f, 1.2f);
     [SerializeField, MinMaxSlider(0f, 10f)] Vector2 yVariation = new(0.8f, 1.2f);
     [SerializeField, Range(0f, 10f)] float duration = 0.8f;
+    
     private Image toGrow;
+    private Tween growingTween;
 
     private void Awake()
     {
@@ -19,8 +21,10 @@ public class SplashImageGrow : MonoBehaviour
         }
     }
     void Start() {
-        if(this.toGrow != null){ 
-            this.toGrow.rectTransform.DOScale(new Vector2(this.xVariation.y, this.yVariation.y), this.duration).SetEase(this.easy);
+        if(this.toGrow != null){
+            this.growingTween = this.toGrow.rectTransform.DOScale(new Vector2(this.xVariation.y, this.yVariation.y), this.duration).SetEase(this.easy);
         }
     }
+
+    private void OnDestroy() => this.growingTween?.Kill();
 }
