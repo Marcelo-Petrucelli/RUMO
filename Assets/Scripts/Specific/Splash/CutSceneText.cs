c087b2808cab4247418c50f3c344da063d1f5b5f
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2023-05-27 02:41:25 -0300

    :sparkles: Feat: Wobble text animation

--- Assets/Scripts/Specific/Splash/CutSceneText.cs
+++ Assets/Scripts/Specific/Splash/CutSceneText.cs
@@ -1,18 +1,37 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CutSceneText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField, BoxGroup("Texts")] public List<TextMeshProUGUI> texts;    
+
+    [SerializeField, BoxGroup("Text transition config")] public float initialWaiting = 1f;
+    [SerializeField, BoxGroup("Text transition config")] public float textWaitingInterval = 1f;
+    [SerializeField, BoxGroup("Text transition config")] public float textFadeInDuration = 1f;   
+
+    private void Start()
+    {        
+        var sequence = DOTween.Sequence();
+        sequence.AppendInterval(initialWaiting);
+        foreach (var text in this.texts) {
+            sequence.Append(TextAnimation(text));
+            sequence.AppendInterval(textWaitingInterval);
+        }       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Tween TextAnimation(TextMeshProUGUI currentText) {        
+        currentText.gameObject.SetActive(true);        
+        var originalColor = currentText.color;
+        var transp = currentText.color - Color.black;
+        currentText.color = transp;
+
+        return currentText.DOColor(originalColor, textFadeInDuration).OnComplete(() => {
+            currentText.GetComponent<Wobble>().WobbleOut();
+        });
     }
 }
