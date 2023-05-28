1f95f7091328cdd534cd36ccb511abed88ed5366
Marcelo Petrucelli <m2petrucelli@gmail.com>
2023-05-28 14:39:59 -0300

    Quick fix em valores do Whirlpool e Fish. Também ajustes no menu..

--- Assets/Scripts/Specific/PauseMenuController.cs
+++ Assets/Scripts/Specific/PauseMenuController.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class PauseMenuController: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, BoxGroup("Config")] private float finalY = 0f;
+    [SerializeField, BoxGroup("Config")] private float animationDuration = 1f;
+
+    private float initialY;
+    private RectTransform myRect;
+    private Tween animation;
+
+    private void Start()
     {
-        
+        this.myRect = GetComponent<RectTransform>();
+        this.initialY = this.myRect.anchoredPosition.y;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Button]
+    public void OpenMenu() {
+        this.animation?.Kill();
+        this.animation = this.myRect.DOAnchorPosY(this.finalY, this.animationDuration, true).SetUpdate(UpdateType.Normal, true);
+    }
+
+    [Button]
+    public void CloseMenu() {
+        this.animation?.Kill();
+        this.animation = this.myRect.DOAnchorPosY(this.initialY, this.animationDuration, true).SetUpdate(UpdateType.Normal, true);
     }
 }
